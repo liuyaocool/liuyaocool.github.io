f60204b6ce91724def4928813deba815ebcbc524
liuyao <liuyao.cool.61@gmail.com>
2024-06-17 14:10:49 +0800

    qrcode lib

--- js/base.js
+++ js/base.js
@@ -91,3 +91,18 @@ function loadRemoteBody(htmlUrl) {
     let doc = new DOMParser().parseFromString(xhr.responseText, 'text/html');
     document.body.innerHTML = doc.body.innerHTML;
 }
+
+function asyncLoadJs(url, scriptOnload) {
+    setTimeout(() => {
+        let t = Date.now();
+        var script = document.createElement('script');
+        script.src = url;
+        script.defer = true;
+        document.body.appendChild(script);
+        if(isFunc(scriptOnload)) {
+            script.onload = function(e) {
+                scriptOnload(e, Date.now() - t);
+            }
+        }
+    }, 500);
+}
